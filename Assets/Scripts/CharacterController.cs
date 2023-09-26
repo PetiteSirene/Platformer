339115b4eee0b27de38645f178850f2f15e2da68
Ariel <ariel.lissak@gmail.com>
2023-09-26 11:28:12 +0200

    Upgrade version unity

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,13 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.HID;
 
 public class CharacterController : MonoBehaviour
 {
-    public float gravityForce, speedMax;
+    public float gravityForce, speedMax, jumpForce;
     
-    private Vector2 move, inputMove;
-    private bool isGrounded;
+    private Vector2 gravityMove, inputMove, jumpMove;
+    private bool isGrounded, canJump;
     
     // Start is called before the first frame update
     void Start()
@@ -20,28 +22,36 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(move + inputMove);
+        transform.Translate(gravityMove + inputMove + jumpMove);
         Gravity();
     }
 
-    private void Movement()
-    {
-        
-    }
-    
     private void Gravity()
     {
         if (!isGrounded)
         {
-            move = Vector2.down * gravityForce;
+            gravityMove = Vector2.down * gravityForce;
         }
         else
         {
-            move = Vector2.zero;
+            gravityMove = Vector2.zero;
         }
+    }
 
+    //Method appelée en Event par l'InputSystem 
+    public void Jump(InputAction.CallbackContext context)
+    {
+        // if (context.phase == InputActionPhase.Started & canJump)
+        //     jumpMove.y = context.ReadValue<float>() * jumpForce;
+        //
+        // if (context.phase == InputActionPhase.Canceled)
+        // {
+        //     jumpMove.y = 0;
+        // }
     }
     
+    
+    //Method appelée en Event par l'InputSystem 
     public void Move(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started)
@@ -50,20 +60,25 @@ public class CharacterController : MonoBehaviour
         }
         if (context.phase == InputActionPhase.Performed)
         {
-            inputMove = context.ReadValue<Vector2>();
+            inputMove.x = context.ReadValue<Vector2>().x;
         }
         if (context.phase == InputActionPhase.Canceled)
         {
-            inputMove = Vector2.zero;
+            inputMove.x = 0;
         }
 
         inputMove *= speedMax;
 
     }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        canJump = true;
+    }
     private void OnCollisionExit2D(Collision2D other)
     {
         isGrounded = false;
+        canJump = false;
     }
     
     private void OnCollisionStay2D(Collision2D other)
