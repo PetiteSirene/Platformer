628a9eeea4b3f142438fd91338e68f518d2584cf
Ariel <ariel.lissak@gmail.com>
2023-09-26 10:40:03 +0200

    Gravity

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,11 @@ using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
+    public float gravityForce, speedMax;
+    
+    private Vector2 move, inputMove;
+    private bool isGrounded;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +20,54 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(move + inputMove);
+        Gravity();
+    }
+
+    private void Movement()
+    {
         
+    }
+    
+    private void Gravity()
+    {
+        if (!isGrounded)
+        {
+            move = Vector2.down * gravityForce;
+        }
+        else
+        {
+            move = Vector2.zero;
+        }
+
     }
     
     public void Move(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValue<Vector2>());
+        if (context.phase == InputActionPhase.Started)
+        {
+            
+        }
+        if (context.phase == InputActionPhase.Performed)
+        {
+            inputMove = context.ReadValue<Vector2>();
+        }
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            inputMove = Vector2.zero;
+        }
+
+        inputMove *= speedMax;
+
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        isGrounded = false;
+    }
+    
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        isGrounded = true;
     }
 }
